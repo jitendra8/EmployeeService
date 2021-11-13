eb743b05da407662ab2e4e96901a507d6e0fe338
Jitendra Kumar Sahu <jitendraks8796@outlook.com>
2021-11-13 17:38:00 +0530

    Add project files.

---
+++ EmployeeService/Scripts/GoogleAuthentication.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="jquery-3.4.1.min.js" />
+
+function getAccessToken() {
+    if (location.hash) {
+        if (location.hash.split('access_token=')) {
+            var accessToken = location.hash.split('access_token=')[1].split('&')[0];
+            if (accessToken) {
+                isUserRegistered(accessToken);
+            }
+        }
+    }
+}
+
+function isUserRegistered(accessToken) {
+    $.ajax({
+        url: '/api/Account/UserInfo',
+        method: 'GET',
+        headers: {
+            'content-type': 'application/JSON',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (response) {
+            if (response.HasRegistered) {
+                sessionStorage.setItem('accessToken', accessToken);
+                sessionStorage.setItem('userName', response.Email);
+                window.location.href = "Data.html";
+            }
+            else {
+                signupExternalUser(accessToken);
+            }
+        }
+    });
+}
+
+function signupExternalUser(accessToken) {
+    $.ajax({
+        url: '/api/Account/RegisterExternal',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function () {          
+            window.location.href = "/api/Account/ExternalLogin?provider=Google&response_type=token&client_id=self&redirect_uri=https%3A%2F%2Flocalhost%3A44373%2FLogin.html&state=SVyDCCKEqKG3bv5kdAts637aok4sPpcKOQ3WQ0JcgEo1";
+        }
+    });
+
+}
